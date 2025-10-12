be6a3cd724d1677cde682b48b5749f1707cb06bf
noachilles <kej010803@gmail.com>
2025-10-12 14:27:04 +0900

    feat(PY-122): Course, User Table 구성

--- back-end/accounts/models.py
+++ back-end/accounts/models.py
@@ -1,14 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class Account(models.Model):
-    id = models.CharField()
-    password = models.CharField()
-    name = models.CharField()
-    phone_number = models.CharField()
-    email = models.EmailField()
-    profile_image_url = models.URLField()
-    introduction = models.CharField()
-    created_at = models.DateField(auto_created=True)
-    
-# 별도의 tutee 테이블이 필요하지는 않을 것 같다는 생각이 들었음
+    username = models.CharField(max_length=20, null=False)
+
+    def __str__(self):
+        return self.username
--- back-end/courses/models.py
+++ back-end/courses/models.py
@@ -1,25 +1,52 @@
 from django.db import models
 from accounts.models import Account
-import Course, Category
 
-# Create your models here.
-class Course(models.Model):
-    title = models.CharField()
-    thumbnail_image_url = models.CharField()
-    introduction = models.CharField()
-    curriculum = models.CharField()
-    price = models.IntegerField()
-    max_tutees = models.IntegerField()
-    current_tutees = models.IntegerField()
-    status = models.BooleanField()
-    created_at = models.DateField(auto_created=True)
-    updated_at = models.DateField(auto_now=True)
-    
-    # tutor가 사라지면 기존 tutor가 만들어둔 내용도 사라짐
-    tutor_id = models.ForeignKey('accounts.Account', on_delete=models.CASCADE)
-    tutees_id_lst = models.ManyToManyField('accounts.Account')
-    
-    # tutor_id = tutor.
-    
 class Category(models.Model):
     name = models.CharField()
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    # 강의 상태 정의  
+    class StatusChoices(models.TextChoices):
+        PREPARING = 'preparing', '준비중' 
+        RECRUITING = 'recruiting', '모집중'
+        IN_PROGRESS = 'in_progress', '강의중'
+        FINISHED = 'finished', '종료'
+    
+    # 기본 정보
+    title = models.CharField(max_length=200, null=False)
+    thumbnail_image_url = models.URLField(max_length=500, null=False)
+    introduction = models.CharField(null=False)
+    curriculum = models.CharField(null=False)
+    price = models.IntegerField(null=False)
+    max_tutees = models.IntegerField(null=False)
+    
+    # 상태, 카운트
+    status = models.CharField(max_length=20, choices=StatusChoices, default=StatusChoices.PREPARING)
+    view_count = models.IntegerField(default=0)
+    
+    # 날짜/시간
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    
+    # 강사(1:N) - 역참조 이름은 created_courses
+    tutor = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='created_courses')
+
+    # inprogress -> tutees (수강생들) - attending_courses
+    tutees = models.ManyToManyField(Account, related_name='attending_courses', blank=True)
+
+    # wish -> wish_users (찜한 유저들) - wished_courses
+    wish_users = models.ManyToManyField(Account, related_name='wished_courses', blank=True)
+
+
+    def __str__(self):
+        return f'[{self.tutor.username}] {self.title}'
+    
+    @property
+    def current_tutees_count(self):
+        # 현재 수강생 수 반환
+        return self.tutees.count()
+
+    # 나중에 리뷰 추가할 시, 리뷰와의 관계도 고려해야 함
