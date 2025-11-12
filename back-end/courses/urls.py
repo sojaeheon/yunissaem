f216ab6b43022aca577573f613bcdf557e30afa8
소재헌 <sojaeheon123@gmail.com>
2025-11-12 16:25:34 +0900

    chore(PY-133) : 카테고리별 과외 urls 수정

--- back-end/courses/urls.py
+++ back-end/courses/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import popular_view, new_view
+from .views import popular_view, new_view,course_list_by_category
 
 urlpatterns = [
     path('popular/', popular_view),
-    path('new/', new_view)
+    path('new/', new_view),
+    path('category/<int:category_id>',course_list_by_category)
 ]
