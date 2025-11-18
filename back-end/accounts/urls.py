b982926aad510796fa761ed16e911e6c8cf34812
소재헌 <sojaeheon123@gmail.com>
2025-11-18 20:54:50 +0900

    D

--- back-end/accounts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import home_view
-
-urlpatterns = [
-    path('home/', home_view)
-]
--- my-app/App.js
+++
@@ -1,5 +0,0 @@
-import RootNavigator from './src/navigation/RootNavigator';
-
-export default function App() {
-  return <RootNavigator />;
-}
