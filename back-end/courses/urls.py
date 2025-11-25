d52dad5ea7d2a4252ec46cbcd01363e039a61dd1
소재헌 <sojaeheon123@gmail.com>
2025-11-25 16:57:55 +0900

    feat(PY-141) : 과외생성api와 courses model 수정
    - 과외생성 api 구현
    - courses 의 이미지업로드 model 수정

--- back-end/courses/urls.py
+++ back-end/courses/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CourseDetailView, CourseWishToggleView, CourseStatusUpdateView, course_list_by_category, search_courses
+from .views import CourseDetailView, CourseWishToggleView, CourseStatusUpdateView, course_list_by_category, search_courses, create_course
 
 urlpatterns = [
     path('<int:course_id>/', CourseDetailView.as_view(), name='course-detail'),
     path('<int:course_id>/wish/', CourseWishToggleView.as_view(), name='course-like-toggle'),
     path('<int:course_id>/status/', CourseStatusUpdateView.as_view(), name='course-status-update'),
     path('category/<int:category_id>',course_list_by_category),
-    path('search/',search_courses)
+    path('search/',search_courses),
+    path('create/',create_course, name='course-create')
 ]
